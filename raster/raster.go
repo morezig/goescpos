8cf65f55de4cb3c9fd2d9ebd3c0b5d85a9ce9d4d
boyce <boyce@morezig.com>
2022-06-25 17:11:41 +0800

    add

--- raster/raster.go
+++ raster/raster.go
@@ -20,12 +20,12 @@ type Converter struct {
 func (c *Converter) Print(img image.Image, target Target) {
 	sz := img.Bounds().Size()
 
-	grayImg := image.NewGray(img.Bounds())
-	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
-		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
-			grayImg.Set(x, y, img.At(x, y))
-		}
-	}
+	// grayImg := image.NewGray(img.Bounds())
+	// for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+	// 	for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+	// 		grayImg.Set(x, y, img.At(x, y))
+	// 	}
+	// }
 
 	data, rw, bw := c.ToRaster(grayImg)
 
@@ -49,26 +49,28 @@ func (c *Converter) ToRaster(img image.Image) (data []byte, imageWidth, bytesWid
 
 	data = make([]byte, bytesWidth*sz.Y)
 
-	// for y := 0; y < sz.Y; y++ {
-	// 	for x := 0; x < imageWidth; x++ {
-	// 		if lightness(img.At(x, y)) >= c.Threshold {
-	// 			// position in data is: line_start + x / 8
-	// 			// line_start is y * bytesWidth
-	// 			// then 8 bits per byte
-	// 			data[y*bytesWidth+x/8] |= 0x80 >> uint(x%8)
-	// 		}
-	// 	}
-	// }
+	for y := 0; y < sz.Y; y++ {
+		for x := 0; x < imageWidth; x++ {
+			if lightness(img.At(x, y)) >= c.Threshold {
+				// position in data is: line_start + x / 8
+				// line_start is y * bytesWidth
+				// then 8 bits per byte
+				data[y*bytesWidth+x/8] |= 0x80 >> uint(x%8)
+			}
+		}
+	}
 
 	return
 }
 
 const (
-	lumR, lumG, lumB = 55, 182, 18
+	// lumR, lumG, lumB = 55, 182, 18  // 反白
+	lumR, lumG, lumB, lumA = 299, 587, 114, 500
 )
 
 func lightness(c color.Color) float64 {
 	r, g, b, _ := c.RGBA()
 
-	return float64(lumR*r+lumG*g+lumB*b) / float64(0xffff*(lumR+lumG+lumB))
+	// return float64(lumR*r+lumG*g+lumB*b) / float64(0xffff*(lumR+lumG+lumB))
+	return float64((lumR*r+lumG*g+lumB*b)+lumA) / float64(0xffff*(lumR+lumG+lumB))
 }
